331dd2fc00037ef09358a128af4946ecf4d59d8f
Jakub Sawicki <j.sawicki@cyfronet.pl>
2021-11-15 17:41:05 +0100

    Remove old skeleton files

--- old/frontend/src/pages/Dashboard.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import { Card, CardHeader } from "@material-ui/core";
-
-export default () => (
-  <Card>
-    <CardHeader title="Welcome to admin" />
-  </Card>
-);
