2c5cc73d20f41088080de0e163ffb59331c38f48
Jakub Sawicki <j.sawicki@cyfronet.pl>
2022-02-08 11:29:17 +0100

    feat: expose facets in search endpoint
    Switch to Solr's JSON Request API and use JSON Facet API
    for the facets. In this way all parameters are passed to Solr
    by JSON request body.

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -5,7 +5,7 @@ from app.routes.mocks import router as mocks_router
 from app.routes.web_auth import router as auth_router
 
 from .recommend import recommend_post
-from .search import search_get
+from .search import search_post
 from .util import internal_api_router
 
 web_api_router = APIRouter()
