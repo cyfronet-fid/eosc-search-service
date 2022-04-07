a919128cad5db04d3dd29df55b42087aa43b9395
Jakub Sawicki <j.sawicki@cyfronet.pl>
2022-04-07 13:31:39 +0200

    test(backend): add integration tests for search

--- backend/tests/app/routes/test_search.py
+++ backend/tests/app/routes/test_search.py
@@ -1,13 +1,20 @@
 # pylint: disable=missing-module-docstring,missing-function-docstring,redefined-outer-name
 import json
 import os
+import unittest.mock
 from unittest.mock import ANY, AsyncMock, Mock, create_autospec
 
 import pytest
 from fastapi import FastAPI
 from httpx import AsyncClient
-from starlette.status import HTTP_200_OK, HTTP_422_UNPROCESSABLE_ENTITY
+from starlette.status import (
+    HTTP_200_OK,
+    HTTP_404_NOT_FOUND,
+    HTTP_422_UNPROCESSABLE_ENTITY,
+    HTTP_500_INTERNAL_SERVER_ERROR,
+)
 
+from app.config import SOLR_URL
 from app.schemas.search_request import TermsFacet
 from app.solr.operations import search, search_dep
 
@@ -125,6 +132,100 @@ async def test_passes_all_facets(
             )
         },
     )
+
+
+@pytest.mark.asyncio
+@pytest.mark.integration
+@pytest.mark.parametrize("collection", ["test_collection"])
+# pylint: disable=unused-argument
+async def test_integration(
+    app: FastAPI, client: AsyncClient, index_solr_docs: None, collection: str
+) -> None:
+    res = await client.post(
+        app.url_path_for("apis:post-search"),
+        params={
+            "q": "model",
+            "collection": collection,
+            "qf": ["title", "description", "subject"],
+        },
+        json={
+            "facets": {
+                "sub": {
+                    "type": "terms",
+                    "field": "subject",
+                    "limit": 5,
+                }
+            }
+        },
+    )
+
+    assert res.status_code == HTTP_200_OK
+    res_json = res.json()
+    assert len(res_json["results"]) == 2
+    assert (
+        res_json["nextCursorMark"]
+        == "AoIIP1qOLj8SNTB8MzU1ZTY1NjI1Yjg4OjowYTU1Yjg3ODA5MTljZTBkNWVhMGU2ZWYxZDhjMDI0MQ=="
+    )
+    assert len(res_json["facets"]["sub"]["buckets"]) == 5
+
+
+@pytest.mark.asyncio
+@pytest.mark.integration
+async def test_integration_400(app: FastAPI, client: AsyncClient) -> None:
+    res = await client.post(
+        app.url_path_for("apis:post-search"),
+        params={
+            "q": "*",
+            "collection": "test_collection",
+        },
+        json={},
+    )
+
+    assert res.status_code == HTTP_404_NOT_FOUND
+    assert res.json() == {"detail": "Not Found"}
+
+
+@pytest.mark.asyncio
+@pytest.mark.integration
+@unittest.mock.patch("app.config.SOLR_URL", "http://localhost:8994/solr/")
+async def test_integration_500(app: FastAPI, client: AsyncClient) -> None:
+    res = await client.post(
+        app.url_path_for("apis:post-search"),
+        params={
+            "q": "*",
+            "collection": "test_collection",
+        },
+        json={},
+    )
+
+    assert res.status_code == HTTP_500_INTERNAL_SERVER_ERROR
+    assert res.json() == {"detail": "Try again later"}
+
+
+@pytest.fixture
+def setup_solr_collection(collection: str) -> None:
+    config_name = "oa_prod_20211208_v2"
+    solr_url = SOLR_URL.replace("/solr/", "")
+
+    os.system(
+        f"../solr/create-collection.sh --name {collection}"
+        f" --config-name {config_name} --solr-url {solr_url}"
+    )
+    yield
+    os.system(f"../solr/delete-collection.sh --name {collection} --solr-url {solr_url}")
+
+
+@pytest.fixture
+# pylint: disable=unused-argument
+async def index_solr_docs(setup_solr_collection: None, collection: str) -> None:
+    with open(f"{os.path.dirname(__file__)}/records.jsonl", "r", encoding="utf-8") as f:
+        lines = f.readlines()
+    request_body = f"[{','.join(lines)}]"
+    async with AsyncClient() as client:
+        await client.post(
+            f"{SOLR_URL}{collection}/update/json/docs?commit=true",
+            content=request_body,
+        )
 
 
 @pytest.fixture
