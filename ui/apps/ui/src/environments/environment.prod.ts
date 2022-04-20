5ceca487560c240603ddd20d305584dec33b787d
Daniel Kryska <daniel.kryska94@gmail.com>
2022-04-20 11:31:21 +0200

    chore: fix prod env

--- ui/apps/ui/src/environments/environment.prod.ts
+++ ui/apps/ui/src/environments/environment.prod.ts
@@ -1,9 +1,6 @@
 export const environment = {
   production: true,
-  backend: {
-    url: 'https://api.eosc-search-service.grid.cyfronet.pl',
-    apiPath: 'api/web',
-  },
+  backendApiPath: 'api/web',
   search: {
     collection: 'oag_researchoutcomes_prod_20211208_v2',
     apiPath: 'search-results',
