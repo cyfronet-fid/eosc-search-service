9085f225bd72433fa7114e604d782d00bf5a756c
Daniel Kryska <daniel.kryska94@gmail.com>
2022-05-16 11:14:51 +0200

    feat: add landing page

--- ui/apps/ui/src/app/articles-page/article.interface.ts
+++ ui/apps/ui/src/app/articles-page/article.interface.ts
@@ -1,7 +1,14 @@
 export interface IArticle {
   id: string;
   title: string;
-  author_names: string[];
+  author_names: string[]; // examples: Hugo, William, Harry
   description: string[];
+  journal: string; // examples: PLoS One, ChemInform
+  publisher: string; // examples: Elsevier BV, Wiley
+  project_titles: string; // examples: Graduate Research Fellowship Program (GRFP), Theoretical Physics
+  subject: string; // examples: Computational, Computer science
+  access_right: string; // example: open access
+  language: string; // example: English
+  published: string; // example: 2016-02-08
   rating?: number;
 }
