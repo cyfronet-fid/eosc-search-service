4199e35db5fc02366f232181c550cccee59d6ce8
Daniel Kryska <daniel.kryska94@gmail.com>
2022-05-25 08:54:16 +0200

    fix: fix input search clear btn, fix active filters visibility

--- ui/libs/pages/search/src/lib/components/active-filters/active-filters.component.ts
+++ ui/libs/pages/search/src/lib/components/active-filters/active-filters.component.ts
@@ -14,7 +14,7 @@ interface IActiveFilter {
 @Component({
   selector: 'ess-active-filters',
   template: `
-    <section *ngIf="$any(activeFilters$ | async)?.length > 0" id="filters">
+    <section *ngIf="$any(activeFilters$ | async)?.length > 0 || (q$ | async) !== '*'" id="filters">
       <span
         id="clear-all-badge"
         style="cursor: pointer"
