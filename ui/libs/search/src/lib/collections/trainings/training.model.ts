8aac6f5b35b6f6c03ce111f726d24e4be82562c6
Daniel Kryska <31220811+danielkryska@users.noreply.github.com>
2022-06-20 11:03:15 +0200

    feat: new training interface

--- ui/libs/search/src/lib/collections/trainings/training.model.ts
+++ ui/libs/search/src/lib/collections/trainings/training.model.ts
@@ -1,8 +1,21 @@
 export interface ITraining {
   id: string;
   "Resource title": string;
+  Description: string;
+  Author: string;
+  Language: string;
+  Keywords: string;
+  License: string;
+  "Access Rights": string;
+  "Version date (created in)": string;
   "Resource Type": string;
   "Content Type": string;
-  "Pilot Catalogue": string;
   URL: string;
+  "EOSC PROVIDER": string;
+  Format: string;
+  "Level of expertise": string;
+  "Target group": string;
+  "Qualification": string;
+  Duration: string;
+  [field: string]: string;
 }
