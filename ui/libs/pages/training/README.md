1a55db0b161de149e814c1b116802eb0f243741d
Michał Szostak <szostak.m.f@gmail.com>
2022-07-11 11:34:23 +0200

    feat: [#140] Training Detail Page
    Closes: #140

---
+++ ui/libs/pages/training/README.md
@@ -0,0 +1,7 @@
+# pages-training
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test pages-training` to execute the unit tests.
---
+++ ui/libs/pages/training/src/lib/pages/training-detail-page/training-detail-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ess-training-detail-page',
+  templateUrl: './training-detail-page.component.html',
+  styleUrls: ['./training-detail-page.component.scss']
+})
+export class TrainingDetailPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
