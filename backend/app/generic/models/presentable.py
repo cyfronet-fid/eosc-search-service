3b3d2b4d50e41c352d7c385e16692b6d0f589744
Pawel Gorczyca <p.gorczyca@cyfronet.pl>
2022-08-10 17:30:52 +0200

    fix: make optional some Presentable fields
    Change:
    * Author_ss
    * Access_Rights_s
    * Version_date__created_in__s
    * Level_of_expertise_s
    * Target_group_s
    * Qualification_s
    * Duration_s
    
    to optional fields because of missing
    data in solr

--- backend/app/generic/models/presentable.py
+++ backend/app/generic/models/presentable.py
@@ -9,19 +9,19 @@ class Presentable(BaseModel):
     id: int | str
     Resource_title_s: str
     Description_s: str
-    Author_ss: list[str]
+    Author_ss: Optional[list[str]] = None
     Language_s: str
     Keywords_ss: list[str]
     License_s: str
-    Access_Rights_s: str
-    Version_date__created_in__s: str
+    Access_Rights_s: Optional[str] = "open_access"
+    Version_date__created_in__s: Optional[str] = None
     Resource_Type_s: str
     Content_Type_s: str
     URL_s: str
     EOSC_PROVIDER_s: str
     Format_ss: list[str]
-    Level_of_expertise_s: str
-    Target_group_s: str
-    Qualification_s: str
-    Duration_s: str
+    Level_of_expertise_s: Optional[str] = None
+    Target_group_s: Optional[str] = None
+    Qualification_s: Optional[str] = None
+    Duration_s: Optional[str] = None
     facets: Optional[dict] = None
