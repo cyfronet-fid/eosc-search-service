a9ae082102a2006af4bc9d77dfe3755b59221d95
Michał Szostak <szostak.m.f@gmail.com>
2022-09-15 10:15:58 +0200

    feat: add stub redirect for user actions

--- backend/app/routes/web/__init__.py
+++ backend/app/routes/web/__init__.py
@@ -6,9 +6,11 @@ from .auth import router as auth_router
 from .mocks import router as mocks_router
 from .presentable import router as presentable_router
 from .search import router as search_router
+from .user_actions import router as user_actions_router
 
 web_api_router = APIRouter()
 web_api_router.include_router(search_router)
 web_api_router.include_router(auth_router, prefix="/auth", tags=["auth"])
 web_api_router.include_router(mocks_router, tags=["mocks"])
+web_api_router.include_router(user_actions_router, tags=["user_actions"])
 web_api_router.include_router(presentable_router)
---
+++ backend/app/routes/web/user_actions.py
@@ -0,0 +1,12 @@
+# pylint: disable=missing-module-docstring,missing-function-docstring
+from fastapi import APIRouter
+from starlette.responses import RedirectResponse
+
+router = APIRouter()
+
+
+@router.get("/navigate", name="web:navigate")
+async def register_navigation_user_action(url: str):
+    """Registers entering a URL and redirects to the URL"""
+    return RedirectResponse(status_code=303, url=url)
+    # send user action via activemq via background task
