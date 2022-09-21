b71c3ceb5284769e8bc04756736318e9092d451d
Daniel Kryska <daniel.kryska94@gmail.com>
2022-09-21 11:14:25 +0200

    feat: stable range filter

--- ui/apps/ui/src/app/pages/search-page/search-page.module.ts
+++ ui/apps/ui/src/app/pages/search-page/search-page.module.ts
@@ -10,15 +10,15 @@ import { NzSpinModule } from 'ng-zorro-antd/spin';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SearchPageComponent } from './search-page.component';
 
-import { DEFAULT_COLLECTION_ID } from '../../collections/data';
-import { NavConfigResolver } from '../../collections/services/nav-config-resolver.service';
-import { HasDefaultQueryParamGuard } from '../../collections/services/has-default-query-param.guard';
-import { ResultsWithPaginationModule } from '../../components/results-with-pagination/results-with-pagination.module';
-import { SearchBarModule } from '../../components/search-bar/search-bar.module';
-import { CollectionsNavigationModule } from '../../components/collections-navigation/collections-navigation.module';
-import { PageHeaderModule } from '../../components/page-header/page-header.module';
-import { FiltersModule } from '../../components/filters/filters.module';
-import { ActiveFiltersModule } from '../../components/active-filters/active-filters.module';
+import { DEFAULT_COLLECTION_ID } from '@collections/data';
+import { NavConfigResolver } from '@collections/services/nav-config-resolver.service';
+import { HasDefaultQueryParamGuard } from '@collections/services/has-default-query-param.guard';
+import { ResultsWithPaginationModule } from '@components/results-with-pagination/results-with-pagination.module';
+import { SearchBarModule } from '@components/search-bar/search-bar.module';
+import { CollectionsNavigationModule } from '@components/collections-navigation/collections-navigation.module';
+import { PageHeaderModule } from '@components/page-header/page-header.module';
+import { FiltersModule } from '@components/filters/filters.module';
+import { ActiveFiltersModule } from '@components/active-filters/active-filters.module';
 
 @NgModule({
   declarations: [SearchPageComponent],
