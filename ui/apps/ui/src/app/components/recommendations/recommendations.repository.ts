3c3b1ad85524078c07bd2caa1142bc87cc428fd1
Daniel Kryska <daniel.kryska94@gmail.com>
2022-10-05 11:28:03 +0200

    chore: code refactor

---
+++ ui/apps/ui/src/app/components/recommendations/recommendations.repository.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { createStore, select, withProps } from '@ngneat/elf';
+import { RecommendationStateProps } from '@components/recommendations/recommendations.types';
+import { setEntities, withEntities } from '@ngneat/elf-entities';
+import { IResult } from '@collections/repositories/types';
+
+@Injectable({ providedIn: 'root' })
+export class RecommendationsRepository {
+  readonly _store$ = createStore(
+    {
+      name: 'recommendations',
+    },
+    withProps<RecommendationStateProps>({ panelId: 'all', status: 'pending' }),
+    withEntities<IResult>()
+  );
+
+  // ASYNC
+  readonly loading$ = this._store$.pipe(
+    select(({ status }) => status === 'pending')
+  );
+
+  // MUTATORS
+  setEntities(entities: IResult[]) {
+    this._store$.update(setEntities(entities));
+  }
+}
--- ui/apps/ui/src/app/components/recommendations/recommendations.service.ts
+++ ui/apps/ui/src/app/components/recommendations/recommendations.service.ts
@@ -1,36 +1,22 @@
 import { Injectable } from '@angular/core';
-import { createStore, select, withProps } from '@ngneat/elf';
-import {
-  IRecommendation,
-  RecommendationStateProps,
-} from '@components/recommendations/recommendations.types';
-import { setEntities, withEntities } from '@ngneat/elf-entities';
+import { IRecommendation } from '@components/recommendations/recommendations.types';
 import { UserProfileService } from '../../auth/user-profile.service';
 import { Observable, catchError, map, of, switchMap, tap } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '@environment/environment';
 import { allCollectionsAdapter } from '@collections/data/all/adapter.data';
 import { IResult } from '@collections/repositories/types';
+import { RecommendationsRepository } from '@components/recommendations/recommendations.repository';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RecommendationsService {
-  readonly _store$ = createStore(
-    {
-      name: 'recommendations',
-    },
-    withProps<RecommendationStateProps>({ panelId: 'all', status: 'pending' }),
-    withEntities<IResult>()
-  );
-
-  // ASYNC
-  readonly loading$ = this._store$.pipe(
-    select(({ status }) => status === 'pending')
-  );
+  loading$ = this._recommendationsRepository.loading$;
 
   constructor(
     private _userProfileService: UserProfileService,
+    private _recommendationsRepository: RecommendationsRepository,
     private _http: HttpClient
   ) {}
 
@@ -40,19 +26,19 @@ export class RecommendationsService {
         if (user.username === '') {
           return of([]) as Observable<IResult[]>;
         }
-        return this._http
-          .get<IRecommendation[]>(
-            `${environment.backendApiPath}/${environment.recommendationsApiPath}?panel_id=${panelId}`
-          )
-          .pipe(
-            catchError(() => of([])),
-            map((recommendations) =>
-              recommendations.map((r) => allCollectionsAdapter.adapter(r))
-            ),
-            tap((recommendations) =>
-              this._store$.update(setEntities(recommendations))
+
+        const url = `${environment.backendApiPath}/${environment.recommendationsApiPath}?panel_id=${panelId}`;
+        return this._http.get<IRecommendation[]>(url).pipe(
+          catchError(() => of([])),
+          map((recommendations) =>
+            recommendations.map((recommendation) =>
+              allCollectionsAdapter.adapter(recommendation)
             )
-          );
+          ),
+          tap((recommendations) =>
+            this._recommendationsRepository.setEntities(recommendations)
+          )
+        );
       })
     );
   }
