deafe46a1bdc8576405a0d7b4a3a2e1e0d52df39
Michał Szostak <szostak.m.f@gmail.com>
2022-10-14 13:40:36 +0200

    feat: [#228] Add privacy policy and terms of use
    * Add static page for privacy policy
    * Add static page for terms of use
    
    Closes #228

---
+++ ui/apps/ui/src/app/pages/privacy-policy/privacy-policy.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ess-privacy-policy',
+  templateUrl: './privacy-policy.component.html',
+  styleUrls: ['./privacy-policy.component.scss'],
+})
+export class PrivacyPolicyComponent {}
