45602aff83a7a77e64be92fdc49ddb33f47671e1
Michał Szostak <szostak.m.f@gmail.com>
2023-01-04 15:00:31 +0100

    feat: [#407] add disclaimer about seearch engine
    Closes #407

--- ui/apps/ui/src/app/components/search-bar/search-bar.component.ts
+++ ui/apps/ui/src/app/components/search-bar/search-bar.component.ts
@@ -20,8 +20,43 @@ import { Component } from '@angular/core';
             <ess-search-input></ess-search-input>
           </div>
         </div>
+        <p class="user-announcement" i18n>
+          We're currently working on improving search engine. We test the best
+          solutions, which may also be associated with some problems. We are
+          open to all suggestions, if you want to report any issues -
+          <a href="https://eosc-portal.eu/contact-us" target="_blank"
+            >Contact us</a
+          >
+        </p>
       </div>
     </div>
   `,
+  styles: [
+    `
+      p.user-announcement {
+        text-align: center;
+        font-size: 11px;
+        color: white;
+        margin-bottom: 20px;
+        max-width: 700px;
+        margin-right: auto;
+        margin-left: auto;
+      }
+
+      p.user-announcement a {
+        color: white;
+        text-decoration: underline;
+      }
+
+      p.user-announcement a:hover {
+        opacity: 0.9;
+        text-decoration: underline;
+      }
+
+      .search-bar {
+        height: 170px;
+      }
+    `,
+  ],
 })
 export class SearchBarComponent {}
