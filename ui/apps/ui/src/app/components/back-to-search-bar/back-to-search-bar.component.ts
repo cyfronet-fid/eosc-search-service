79a037b83616e5748b03f88ef3c84b1ec3ac5b29
Jarek Zbozień <j.zbozien@gmail.com>
2023-03-09 15:09:09 +0100

    feat: [#468] Consistent look and feel
    * suggestions list RWD fix
    * suggestions list position fix
    * trainings view and search width
    
    Closes #468

--- ui/apps/ui/src/app/components/back-to-search-bar/back-to-search-bar.component.ts
+++ ui/apps/ui/src/app/components/back-to-search-bar/back-to-search-bar.component.ts
@@ -3,26 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ess-back-to-search-bar',
   template: `<div class="eosc-back-search-bar">
-    <div class="container--xxl">
+    <div class="container">
       <div class="col-md-3 col-12 eosc-back-link">
         <div class="chevron-left"></div>
         <a routerLink="/search" i18n>Go to Search</a>
       </div>
     </div>
   </div>`,
-  styles: [
-    `
-      .eosc-back-search-bar {
-        margin-top: -15px;
-        height: 60px;
-        text-decoration: none;
-        font-weight: 100;
-      }
-
-      .eosc-back-link {
-        padding-top: 17px;
-      }
-    `,
-  ],
 })
 export class BackToSearchBarComponent {}
