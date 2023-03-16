362b83f3287ccffd1e96779839ddacf97b40e1db
mich.kolomanski <mich.kolomanski@gmail.com>
2023-03-16 11:22:03 +0100

    feat: [#451, #453] add providers and guidelines

--- transform/README.md
+++ transform/README.md
@@ -1,6 +1,7 @@
 # Create all collection
 ## ENV variables
 We are using .env to store user-specific constants. This file is not tracked by git and it needs to be present in the project directory. Details:
+### Data dump:
 - `DATASET_PATH` - A path to datasets **directory**. Default: `input_data/dataset/`
 - `PUBLICATION_PATH` - A path to publications **directory**. Default: `input_data/publication/`
 - `SOFTWARE_PATH` - A path to software **directory**. Default: `input_data/software/`
@@ -8,11 +9,19 @@ We are using .env to store user-specific constants. This file is not tracked by 
 - `TRAINING_PATH` - A path to trainings **directory**. Default: `input_data/training/`
 - `SERVICE_PATH` - A path to services **directory**. Default: `input_data/service/`
 - `DATASOURCE_PATH` - A path to datasources **directory**. Default: `input_data/datasource/`
-- `OUTPUT_PATH` - Where the output should be locally saved? Note that if the `CREATE_LOCAL_DUMP` variable will be set to `False`, output files will be deleted after each iteration.  Default: `output/`.
+- `PROVIDER_PATH` - A path to providers **directory**. Default: `input_data/provider/`
+
+### Data from API:
+- `GUIDELINE_ADDRESS` - A full address to get all interoperability guidelines **endpoint**. Default: `"https://beta.providers.eosc-portal.eu/api/interoperabilityRecord/all"`
 <br></br>
+
+### Other data related params:
 - `INPUT_FORMAT` - Format of the input data files. Default: `JSON`.
 - `OUTPUT_FORMAT` - Format of the output data files. Default: `JSON`.
+- `OUTPUT_PATH` - Where the output should be locally saved? **IMPORTANT**: during each iteration the whole content of this path will be deleted! Therefore, use some empty path. Default: `output/`.
 <br></br>
+
+### Third party services:
 - `SEND_TO_SOLR` -  A flag which indicates if data should be sent to Solr. Default `True`.
   <br> To send data to Solr it is needed to provide also:
   - `SOLR_ADDRESS` - Solr address. Default: `http://127.0.0.1`.
@@ -24,6 +33,8 @@ We are using .env to store user-specific constants. This file is not tracked by 
   - `SOLR_TRAINING_COLS` - The name of the collection/collections to which trainings will be sent. Example: `"all training"`.
   - `SOLR_SERVICE_COLS` - The name of the collection/collections to which services will be sent. Example: `"all service"`.
   - `SOLR_DATASOURCE_COLS` - The name of the collection/collections to which datasources will be sent. Example: `"all datasource"`.
+  - `SOLR_PROVIDER_COLS` - The name of the collection/collections to which providers will be sent. Example: `"provider"`.
+  - `SOLR_GUIDELINE_COLS` - The name of the collection/collections to which interoperability guidelines will be sent. Example: `"guideline"`.
 <br></br>
 - `SEND_TO_S3` - A flag which indicates if data should be sent to S3. Default `False`.
   <br> To send data to S3 it is needed to provide also:
--- transform/utils/loader.py
+++ transform/utils/loader.py
@@ -14,6 +14,9 @@ OTHER_RP = "OTHER_RP"
 TRAINING = "TRAINING"
 SERVICE = "SERVICE"
 DATASOURCE = "DATASOURCE"
+PROVIDER = "PROVIDER"
+GUIDELINE = "GUIDELINE"
+
 
 DATASET_PATH = "DATASET_PATH"
 PUBLICATION_PATH = "PUBLICATION_PATH"
@@ -22,10 +25,13 @@ OTHER_RP_PATH = "OTHER_RP_PATH"
 TRAINING_PATH = "TRAINING_PATH"
 SERVICE_PATH = "SERVICE_PATH"
 DATASOURCE_PATH = "DATASOURCE_PATH"
-OUTPUT_PATH = "OUTPUT_PATH"
+PROVIDER_PATH = "PROVIDER_PATH"
+
+GUIDELINE_ADDRESS = "GUIDELINE_ADDRESS"
 
 INPUT_FORMAT = "INPUT_FORMAT"
 OUTPUT_FORMAT = "OUTPUT_FORMAT"
+OUTPUT_PATH = "OUTPUT_PATH"
 
 SEND_TO_SOLR = "SEND_TO_SOLR"
 SOLR_ADDRESS = "SOLR_ADDRESS"
@@ -37,6 +43,8 @@ SOLR_OTHER_RP_COLS = "SOLR_OTHER_RP_COLS"
 SOLR_TRAINING_COLS = "SOLR_TRAINING_COLS"
 SOLR_SERVICE_COLS = "SOLR_SERVICE_COLS"
 SOLR_DATASOURCE_COLS = "SOLR_DATASOURCE_COLS"
+SOLR_PROVIDER_COLS = "SOLR_PROVIDER_COLS"
+SOLR_GUIDELINE_COLS = "SOLR_GUIDELINE_COLS"
 
 SEND_TO_S3 = "SEND_TO_S3"
 S3_ACCESS_KEY = "S3_ACCESS_KEY"
@@ -49,13 +57,15 @@ S3_DUMP_NAME = "S3_DUMP_NAME"
 CREATE_LOCAL_DUMP = "CREATE_LOCAL_DUMP"
 LOCAL_DUMP_PATH = "LOCAL_DUMP_PATH"
 
-COLLECTIONS = "COLLECTIONS"
+ALL_COLLECTION = "ALL_COLLECTION"
+SEPARATE_COLLECTION = "SEPARATE_COLLECTION"
 SOLR_COL_NAMES = "SOLR_COL_NAMES"
 PATH = "PATH"
+ADDRESS = "ADDRESS"
 FIRST_FILE_PATH = "FIRST_FILE_PATH"
 FIRST_FILE_DF = "FIRST_FILE_DF"
 
-solr_col_mapping = {
+solr_all_col_mapping = {
     DATASOURCE: SOLR_DATASOURCE_COLS,
     SERVICE: SOLR_SERVICE_COLS,
     TRAINING: SOLR_TRAINING_COLS,
@@ -64,6 +74,12 @@ solr_col_mapping = {
     PUBLICATION: SOLR_PUBLICATION_COLS,
     DATASET: SOLR_DATASET_COLS,
 }
+
+solr_sep_col_mapping = {
+    PROVIDER: SOLR_PROVIDER_COLS,
+    GUIDELINE: SOLR_GUIDELINE_COLS,
+}
+
 load_dotenv()
 
 
@@ -71,7 +87,7 @@ def load_data(
     spark: SparkSession, data_path: str, col_name: str, _format: str = "json"
 ):
     """Load data based on the provided data path"""
-    if col_name in {SERVICE, DATASOURCE}:
+    if col_name in {SERVICE, DATASOURCE, PROVIDER}:
         return spark.read.format(_format).option("multiline", True).load(data_path)
     return spark.read.format(_format).load(data_path)
 
@@ -102,7 +118,8 @@ def load_env_vars() -> dict:
 
 def load_config(env_vars: dict) -> None:
     """Load Solr config"""
-    env_vars[COLLECTIONS] = create_collection(env_vars[SEND_TO_SOLR])
+    env_vars[ALL_COLLECTION] = load_vars_all_collection(env_vars[SEND_TO_SOLR])
+    env_vars[SEPARATE_COLLECTION] = load_vars_sep_collection(env_vars[SEND_TO_SOLR])
     current_date = str(date.today())
 
     if env_vars[SEND_TO_SOLR]:
@@ -124,8 +141,8 @@ def load_config(env_vars: dict) -> None:
         env_vars[LOCAL_DUMP_PATH] = os.environ.get(LOCAL_DUMP_PATH, current_date)
 
 
-def create_collection(solr_flag: bool) -> dict:
-    """Create collection"""
+def load_vars_all_collection(solr_flag: bool) -> dict:
+    """Load variables for all collection"""
     collections = {
         DATASOURCE: {
             PATH: os.environ.get(DATASOURCE_PATH, "input_data/datasource/"),
@@ -158,7 +175,7 @@ def create_collection(solr_flag: bool) -> dict:
     }
     if solr_flag:
         for col_name, col_val in collections.items():
-            col_val[SOLR_COL_NAMES] = os.environ.get(solr_col_mapping[col_name])
+            col_val[SOLR_COL_NAMES] = os.environ.get(solr_all_col_mapping[col_name])
 
     get_first_file_paths(collections)
 
@@ -170,6 +187,37 @@ def create_collection(solr_flag: bool) -> dict:
         )
 
     return collections
+
+
+def load_vars_sep_collection(solr_flag: bool) -> dict:
+    """Load variables for separate collections"""
+    sep_collections = {
+        PROVIDER: {
+            PATH: os.environ.get(PROVIDER_PATH, "input_data/provider/"),
+        },
+        GUIDELINE: {
+            ADDRESS: os.environ.get(
+                GUIDELINE_ADDRESS,
+                "https://beta.providers.eosc-portal.eu/api/interoperabilityRecord/all",
+            ),
+        },
+    }
+    if solr_flag:
+        for col_name, col_val in sep_collections.items():
+            col_val[SOLR_COL_NAMES] = os.environ.get(solr_sep_col_mapping[col_name])
+
+    if any(
+        (
+            not bool(env_var)
+            for col in sep_collections.values()
+            for env_var in col.values()
+        )
+    ):
+        raise ValueError(
+            f"Not all necessary .env variables were passed. Env = {sep_collections}"
+        )
+
+    return sep_collections
 
 
 def get_first_file_paths(collections: dict) -> None:
