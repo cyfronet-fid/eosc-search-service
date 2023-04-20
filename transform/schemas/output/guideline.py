f776a3ddb083da602aca284734b39df0e60a0324
mich.kolomanski <mich.kolomanski@gmail.com>
2023-04-20 15:30:18 +0200

    fix: subtitle for datasets included

--- transform/schemas/output/guideline.py
+++ transform/schemas/output/guideline.py
@@ -1,4 +1,4 @@
 # pylint: disable=fixme
 """Guideline expected schema after transformations"""
 
-guideline_output_schema = {}  # TODO
+guideline_output_schema = {"TBD"}  # TODO
--- transform/transformers/dataset.py
+++ transform/transformers/dataset.py
@@ -10,9 +10,9 @@ class DatasetTransformer(OagBaseTransformer):
         super().__init__(self.type, self.cols_to_add, self.cols_to_drop, spark)
 
     @property
-    def cols_to_add(self) -> None:
+    def cols_to_add(self) -> tuple[str, ...]:
         """Add those columns to the dataframe"""
-        return None
+        return ("subtitle",)
 
     @property
     def cols_to_drop(self) -> tuple[str, ...]:
