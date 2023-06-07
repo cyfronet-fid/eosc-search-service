ffacdf5783f9bc31c1eb6830b3c271c8923819ac
Michał Szostak <szostak.m.f@gmail.com>
2023-06-07 09:20:33 +0200

    fix: [#638, #626] Fix back to search in trainings
    * Fix back to search in trainings (for multiple
      keywords)
    * Fix selecting keyword from trainings
    
    Closes #638, #626

--- ui/apps/ui/src/app/components/back-to-search-bar/back-to-search-bar.component.ts
+++ ui/apps/ui/src/app/components/back-to-search-bar/back-to-search-bar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
+@UntilDestroy()
 @Component({
   selector: 'ess-back-to-search-bar',
   template: `<div class="eosc-back-search-bar">
@@ -19,17 +21,21 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class BackToSearchBarComponent implements OnInit {
   return_path: string | undefined;
-  parsedQueryParams: { [id: string]: string } = {};
+  parsedQueryParams: { [id: string]: string[] } = {};
 
   constructor(private route: ActivatedRoute) {}
   ngOnInit(): void {
-    this.route.queryParams.subscribe((params) => {
+    this.route.queryParams.pipe(untilDestroyed(this)).subscribe((params) => {
       this.parsedQueryParams = [
         ...new URLSearchParams(
           decodeURIComponent(params['search_params'])
         ).entries(),
-      ].reduce((pv: { [id: string]: string }, cv) => {
-        pv[cv[0]] = cv[1];
+      ].reduce((pv: { [id: string]: string[] }, cv) => {
+        if (pv[cv[0]] !== undefined) {
+          pv[cv[0]].push(cv[1]);
+        } else {
+          pv[cv[0]] = [cv[1]];
+        }
         return pv;
       }, {});
       this.return_path = params['return_path'];
