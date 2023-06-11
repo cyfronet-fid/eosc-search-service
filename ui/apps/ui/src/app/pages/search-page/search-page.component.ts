2ea2b8f913ae004259a1eb4bf5147af925c8f03e
Artur Florian <aflorian@sii.pl>
2023-06-11 07:32:53 +0200

    fix: #652 bug fixed

--- ui/apps/ui/src/app/pages/search-page/search-page.component.ts
+++ ui/apps/ui/src/app/pages/search-page/search-page.component.ts
@@ -82,7 +82,9 @@ import { queryChanger } from '@collections/filters-serializers/utils';
           <div class="col-sm-9 col-12 center-column">
             <ess-page-header
               [resultsCount]="response?.numFound ?? 0"
-              [sortByOptionOff]="(response?.results ?? [])[0].sortByOptionOff"
+              [sortByOptionOff]="
+                (response?.results ?? [])[0]?.sortByOptionOff ?? false
+              "
             ></ess-page-header>
             <ess-active-filters></ess-active-filters>
 
