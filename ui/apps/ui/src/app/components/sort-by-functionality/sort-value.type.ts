b80bb93e4a639847f4f0d285f1894b2c4a9fd806
mich.kolomanski <mich.kolomanski@gmail.com>
2023-06-13 12:22:07 +0200

    fix: sort by default

--- ui/apps/ui/src/app/components/sort-by-functionality/sort-value.type.ts
+++ ui/apps/ui/src/app/components/sort-by-functionality/sort-value.type.ts
@@ -1,7 +1,7 @@
-export type sortType = 'dmr' | 'dlr' | 'mp';
+export type sortType = 'dmr' | 'dlr' | 'mp' | 'default';
 // export type sortType = 'r' | 'dmr' | 'dlr' | 'mp';
-export const DEFAULT_SORT: sortType = 'mp';
+export const DEFAULT_SORT: sortType = 'default';
 
 export function isSortOption(obj: string): obj is sortType {
-  return ['dlr', 'dmr', 'mp'].includes(obj); // return ['dlr', 'dmr', 'mp', 'r'].includes(obj);
+  return ['dlr', 'dmr', 'mp', 'default'].includes(obj); // return ['dlr', 'dmr', 'mp', 'r'].includes(obj);
 }
