5f4eb310fbd7c26371ab3a3f22a756fc961ab9ce
Michal-Kolomanski <mich.kolomanski@gmail.com>
2023-09-18 17:24:40 +0200

    feat: [#882] default sort -> best match sort

--- ui/apps/ui/src/app/components/sort-by-functionality/sort-value.type.ts
+++ ui/apps/ui/src/app/components/sort-by-functionality/sort-value.type.ts
@@ -1,6 +1,6 @@
-export type sortType = 'dmr' | 'dlr' | 'mp' | 'r' | 'default';
-export const DEFAULT_SORT: sortType = 'default';
+export type sortType = 'dmr' | 'dlr' | 'mp' | 'r' | 'bm';
+export const BEST_MATCH_SORT: sortType = 'bm';
 
 export function isSortOption(obj: string): obj is sortType {
-  return ['dlr', 'dmr', 'mp', 'r', 'default'].includes(obj);
+  return ['dlr', 'dmr', 'mp', 'r', 'bm'].includes(obj);
 }
