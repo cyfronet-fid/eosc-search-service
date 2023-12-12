c6d2a19dce1af6381c5c8e4c949f138c4aeaccfb
Michal-Kolomanski <mich.kolomanski@gmail.com>
2023-12-12 13:51:07 +0100

    feat: [#1121] speed up search

--- backend/tests/app/routes/test_configuration.py
+++ backend/tests/app/routes/test_configuration.py
@@ -16,6 +16,7 @@ async def test_return_backend_config(app: FastAPI, client: AsyncClient) -> None:
         "marketplace_url": "https://marketplace.eosc-portal.eu",
         "eosc_explore_url": "https://explore.eosc-portal.eu",
         "knowledge_hub_url": "https://knowledge-hub.eosc-portal.eu/",
-        "is_sort_by_relevance": False,
-        "max_results_by_page": 250,
+        "is_sort_by_relevance": True,
+        "max_results_by_page": 50,
+        "max_items_sort_relevance": 250,
     }
