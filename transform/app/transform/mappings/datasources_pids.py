bb9e8c4d445798a4d0ca2dd3ca849e3d39a2d66d
Michal-Kolomanski <mich.kolomanski@gmail.com>
2023-12-21 17:11:26 +0100

    feat: [#1127] map oag data sources pids

---
+++ transform/app/transform/mappings/datasources_pids.py
@@ -0,0 +1,43 @@
+"""Mapping of data sources PIDs"""
+
+# TODO Remove the whole mapping in the February data update
+
+datasource_pids_mapping = {
+    "eosc.cessda-eric.7e17e8817404ce7a8013be373723b2be": "eosc.cessda-eric.cdc",
+    "eosc.psi.f1a79f572f95bc2fbea5cdc40ef4eb22": "eosc.psi.psi_public_data_repository",
+    "eosc.uniwersytet_opolski.19b44a96f7a776774de3939d9820d00c": "eosc.uniwersytet_opolski.bk_uniopole",
+    "eosc.lindatclariah-cz.6dc98fcb5294282acf3d92f3ab3376b2": "eosc.lindatclariah-cz.lindatclariah-cz_repository",
+    "eosc.eudat.17bb7bb8ef1af0f9bdb55a7db30cfa8a": "eosc.eudat.b2share",
+    "eosc.acdh-ch.3b0149bee976d6db7eef053159e97a87": "eosc.acdh-ch.arche",
+    "eosc.uit.49e8d4cef23bda3b66dd417e6675727d": "eosc.uit.trolling",
+    "eosc.csuc.135887d3dea4b6723095d13c28dd52a3": "eosc.csuc.corardr",
+    "eosc.ccsd.06cdd3ff4700bb4c8e7bf22c14f23f5b": "eosc.ccsd.episciences",
+    "eosc.gbif.14ac40283813a624bd74ae82605ded23": "eosc.gbif.gbif_species_occurrence_data",
+    "eosc.gdansk_tech.1434de11c83986b5be5592677f28d171": "eosc.gdansk_tech.most",
+    "eosc.gwdg.d6521479ffa922bbccc839606b8ec7c5": "eosc.gwdg.textgrid_repository",
+    "eosc.unipd.12d35bb1f56d4b91bb4644faf76d9486": "eosc.unipd.rdu",
+    "eosc.unibi-ub.a61d9ea844bdf43e6feabd6b14dfe3c5": "eosc.unibi-ub.pub",
+    "eosc.scipedia.0063745e5964b19c3e9ceeb2bd6632f5": "eosc.scipedia.spaosp",
+    "eosc.psnc.6f0470e3bb9203ec3a7553f3a72a7a1f": "eosc.psnc.rohub",
+    "eosc.ill.d422cba59746f39d10bdfea5c9cf8511": "eosc.ill.ill_data_portal",
+    "eosc.ceric-eric.e9354332fd75190b935b80c1ba30b837": "eosc.ceric-eric.ceric-data-portal",
+    "eosc.cnr_-_isti.dbe89d2b83f3e29caab7923a51c1d151": "eosc.cnr_-_isti.isti_open_portal",
+    "eosc.lapp.ef0bb7d889d0cced364444495f7a1e67": "eosc.lapp.ossr",
+    "eosc.lida.26c1ee137e7510fd1d7e44eb87cdb4af": "eosc.lida.lida_survey_data",
+    "eosc.awi_bremerhaven.2882af227241cb956c28fe321a70dfb2": "eosc.awi_bremerhaven.pangaea",
+    "eosc.riga_stradins_university.4ea61809e753e65a459bbe4a492c773b": "eosc.riga_stradins_university.rsu_dataverse",
+    "eosc.ku_leuven.1cb0937dc41e70d8126d7b259ad470af": "eosc.ku_leuven.ku_leuven_rdr",
+    "eosc.dkrz.9ffffb05aaf22e7f9138dca4560a8c8b": "eosc.dkrz.wdcc",
+    "ni4os.ibiss.ibiss_radar": "ni4os.ibiss.ibiss_radar",
+    "eosc.openaire.0a02f13310296033694acead588a773b": "eosc.openaire.zenodo",
+    "eosc.vilnius-university.1ec069c1620d49d460e4cbcec0af57f6": "eosc.vilnius-university.tnoarda",
+    "eosc.icos_eric.25c5f3f0674fb287e05e697263e211e2": "eosc.icos_eric.data_discovery_and_access_portal",
+    "eosc.fris.8f42bfccf70de38b01763b704300f882": "eosc.fris.fris",
+}
+
+services_pids = [
+    "eosc.taltechdata.tallinn_university_of_technology_data_repository",
+    "eosc.openaire.openapc",
+    "ni4os.nsl-ge.digital_repository_of_georgian_scientific_works",
+    "eosc.materialscloud.materials_cloud_archive",
+]
