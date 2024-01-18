ffba0dd00b9f7082f130bfcdaf7b265b256c6a39
Michal-Kolomanski <mich.kolomanski@gmail.com>
2024-01-18 09:56:33 +0100

    feat: preload lazy loaded modules

--- ui/apps/ui/src/app/app.routing.module.ts
+++ ui/apps/ui/src/app/app.routing.module.ts
@@ -1,61 +1,80 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { ErrorPageModule } from '@pages/error-page/error-page.module';
-import { SearchPageModule } from '@pages/search-page/search-page.module';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { SEARCH_PAGE_PATH } from '@collections/services/custom-route.type';
-import { TrainingsPageModule } from '@pages/trainings-page/trainings-page.module';
-import { AcceptableUsePolicyModule } from '@pages/acceptable-use-policy/acceptable-use-policy.module';
-import { PrivacyPolicyModule } from '@pages/privacy-policy/privacy-policy.module';
-import { GuidelinesPageModule } from '@pages/guidelines-page/guidelines-page.module';
-import { AdvSearchGuideModule } from '@pages/adv-search-guide/adv-search-guide.module';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: SEARCH_PAGE_PATH,
+  },
+  {
+    path: `${SEARCH_PAGE_PATH}/all`,
+    pathMatch: 'full',
+    redirectTo: `${SEARCH_PAGE_PATH}/all_collection`,
+  },
+  {
+    path: SEARCH_PAGE_PATH,
+    loadChildren: () =>
+      import('./pages/search-page/search-page.module').then(
+        (m) => m.SearchPageModule
+      ),
+  },
+  {
+    path: 'trainings',
+    loadChildren: () =>
+      import('./pages/trainings-page/trainings-page.module').then(
+        (m) => m.TrainingsPageModule
+      ),
+  },
+  {
+    path: 'guidelines',
+    loadChildren: () =>
+      import('./pages/guidelines-page/guidelines-page.module').then(
+        (m) => m.GuidelinesPageModule
+      ),
+  },
+  {
+    path: 'acceptable-use-policy',
+    pathMatch: 'full',
+    loadChildren: () =>
+      import('./pages/acceptable-use-policy/acceptable-use-policy.module').then(
+        (m) => m.AcceptableUsePolicyModule
+      ),
+  },
+  {
+    path: 'asguide',
+    pathMatch: 'full',
+    loadChildren: () =>
+      import('./pages/adv-search-guide/adv-search-guide.module').then(
+        (m) => m.AdvSearchGuideModule
+      ),
+  },
+  {
+    path: 'privacy-policy',
+    pathMatch: 'full',
+    loadChildren: () =>
+      import('./pages/privacy-policy/privacy-policy.module').then(
+        (m) => m.PrivacyPolicyModule
+      ),
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: 'error/404',
+  },
+  {
+    path: 'error',
+    loadChildren: () =>
+      import('./pages/error-page/error-page.module').then(
+        (m) => m.ErrorPageModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot([
-      {
-        path: '',
-        pathMatch: 'full',
-        redirectTo: SEARCH_PAGE_PATH,
-      },
-      {
-        path: `${SEARCH_PAGE_PATH}/all`,
-        pathMatch: 'full',
-        redirectTo: `${SEARCH_PAGE_PATH}/all_collection`,
-      },
-      {
-        path: SEARCH_PAGE_PATH,
-        loadChildren: () => SearchPageModule,
-      },
-      {
-        path: 'trainings',
-        loadChildren: () => TrainingsPageModule,
-      },
-      {
-        path: 'guidelines',
-        loadChildren: () => GuidelinesPageModule,
-      },
-      {
-        path: 'acceptable-use-policy',
-        pathMatch: 'full',
-        loadChildren: () => AcceptableUsePolicyModule,
-      },
-      {
-        path: 'asguide',
-        pathMatch: 'full',
-        loadChildren: () => AdvSearchGuideModule,
-      },
-      {
-        path: 'privacy-policy',
-        pathMatch: 'full',
-        loadChildren: () => PrivacyPolicyModule,
-      },
-      {
-        path: '**',
-        pathMatch: 'full',
-        redirectTo: 'error/404',
-      },
-      { path: 'error', loadChildren: () => ErrorPageModule },
-    ]),
+    RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules }),
   ],
   exports: [RouterModule],
 })
