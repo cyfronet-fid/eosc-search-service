f230c036a0ccf7a6e72c1c70d0fbd72445d58128
wiktorflorian <wiktorflorianwf@gmail.com>
2024-03-20 13:16:40 +0100

    feat: [#1275] enhance live update

--- transform/app/services/jms/connector.py
+++ transform/app/services/jms/connector.py
@@ -18,14 +18,36 @@ logger = logging.getLogger(__name__)
 
 conn: stomp.Connection | None = None
 
+# Constant
+INIT_JMS_RETRY_INTERVAL = 60 # seconds
+
 
 def subscription_condition(connection: stomp.Connection) -> bool:
-    """Is it connected?"""
+    """
+    Check if the STOMP connection is currently active.
+
+    Args:
+        connection (stomp.Connection): The STOMP connection object.
+
+    Returns:
+        bool: True if the connection is active, False otherwise.
+    """
     return connection.is_connected()
 
 
 class JMSMessageHandler(stomp.ConnectionListener):
-    """Class for handling incoming JMS messages"""
+    """
+    Class for handling incoming JMS messages.
+
+    Args:
+        host (str): The hostname of the JMS server.
+        port (int): The port number of the JMS server.
+        username (str): Username for authentication.
+        password (str): Password for authentication.
+        topics (Union[str, List[str]]): Topic or list of topics to subscribe to.
+        subscription_id (str): ID for the subscription.
+        _ssl (bool): Flag to indicate if SSL is to be used.
+    """
 
     def __init__(
         self,
@@ -46,14 +68,16 @@ class JMSMessageHandler(stomp.ConnectionListener):
         self._ssl = _ssl
 
     def on_message(self, frame):
-        logger.debug("Received a message '%s'", frame.body)
+        logger.info("%s: Received a message '%s'", self.__class__.__name__, frame.body)
         process_message(frame)
 
     def on_error(self, frame):
-        logger.error("Received an error '%s'", frame.body)
+        logger.error("%s: Received an error '%s'", self.__class__.__name__, frame.body)
 
     def on_disconnected(self):
-        logging.warning("Got disconnected from the queue. Attempting to reconnect...")
+        logging.warning(
+            "%s: Disconnected from the queue. Attempting to reconnect...", self.__class__.__name__
+        )
         subscribe(
             conn,
             self.host,
@@ -76,7 +100,19 @@ def subscribe(
     subscription_id: str,
     _ssl: bool = True,
 ) -> None:
-    """Subscribe to the topics"""
+    """
+    Subscribe to the specified JMS topics.
+
+    Args:
+        connection (stomp.Connection): The STOMP connection object.
+        host (str): The hostname of the JMS server.
+        port (int): The port number of the JMS server.
+        username (str): Username for authentication.
+        password (str): Password for authentication.
+        topics (Union[str, List[str]]): Topic or list of topics to subscribe to.
+        subscription_id (str): ID for the subscription.
+        _ssl (bool): Flag to indicate if SSL is to be used.
+    """
     connection.connect(username=username, password=password, wait=True, ssl=_ssl)
     topics = [topics] if isinstance(topics, str) else topics
 
@@ -96,7 +132,22 @@ async def connect(
     subscription_id: str,
     _ssl: bool = True,
 ) -> None:
-    """Make a connection to the JMS"""
+    """
+    Establishes a connection to the JMS server and subscribes to specified topics.
+
+    Args:
+        host (str): The hostname of the JMS server.
+        port (int): The port number of the JMS server.
+        username (str): Username for authentication.
+        password (str): Password for authentication.
+        topics (Union[str, List[str]]): Topic or list of topics to subscribe to.
+        subscription_id (str): ID for the subscription.
+        _ssl (bool): Flag to indicate if SSL is to be used.
+
+    Raises:
+        ConnectFailedException: If the connection to the JMS server fails.
+        Exception: For general exceptions, usually related to subscription issues.
+    """
     global conn
     try:
         conn = stomp.Connection(host_and_ports=[(host, port)])
@@ -111,22 +162,33 @@ async def connect(
         subscribe(conn, host, port, username, password, topics, subscription_id, _ssl)
 
     except KeyboardInterrupt:
+        logger.warning("KeyboardInterrupt detected, disconnecting from JMS.")
         conn.disconnect()
-    except ConnectFailedException:
-        logger.error(f"Failed to connect to the queue: {host}:{port}")
+    except ConnectFailedException as e:
+        logger.error(f"Failed to connect to the JMS: {host}:{port}, error: {e}")
     except Exception as e:
         logger.error(f"Failed to subscribe to certain topics: {topics}, error: {e}")
 
 
 async def start_jms_subscription():
-    """Start JMS subscription."""
+    """
+    Start a JMS subscription service.
+
+    Continuously attempts to connect to the JMS server and subscribe to specified topics.
+    Retries connection in case of failure with a 1-minute interval.
+
+    Raises:
+        ConnectFailedException: If unable to establish a connection to the JMS server.
+        Exception: For other general exceptions that might occur during the subscription process.
+    """
     if not settings.STOMP_SUBSCRIPTION:
+        logger.info("STOMP subscription is disabled in settings.")
         return
 
     while True:
         try:
             logger.info(
-                f"Trying to subscribe to the queue: {settings.STOMP_HOST}:{settings.STOMP_PORT}, topics: {settings.STOMP_TOPICS}, ssl={settings.STOMP_SSL}"
+                f"Attempting to subscribe to the JMS: {settings.STOMP_HOST}:{settings.STOMP_PORT}, topics: {settings.STOMP_TOPICS}, ssl={settings.STOMP_SSL}"
             )
             await connect(
                 host=settings.STOMP_HOST,
@@ -138,11 +200,16 @@ async def start_jms_subscription():
                 _ssl=settings.STOMP_SSL,
             )
             break
+        except ConnectFailedException as e:
+            logger.error(
+                f"Connection to JMS failed, retrying in {RETRY_INTERVAL_SECONDS} seconds... Queue: {settings.STOMP_HOST}:{settings.STOMP_PORT}, error: {e}"
+            )
+            await asyncio.sleep(RETRY_INTERVAL_SECONDS)
         except Exception as e:
             logger.error(
-                f"Failed to connect to the queue, retrying in 1 minute... Queue: {settings.STOMP_HOST}:{settings.STOMP_PORT}, error: {e}"
+                f"Error during JMS subscription, retrying in {RETRY_INTERVAL_SECONDS} seconds... Queue: {settings.STOMP_HOST}:{settings.STOMP_PORT}, error: {e}"
             )
-            await asyncio.sleep(60)
+            await asyncio.sleep(RETRY_INTERVAL_SECONDS)
 
 
 async def close_jms_subscription():
--- transform/app/transform/live_update/training_ig.py
+++ transform/app/transform/live_update/training_ig.py
@@ -13,7 +13,15 @@ logger = logging.getLogger(__name__)
 
 
 def process_message(frame) -> None:
-    """Process messages from a JMS"""
+    """
+    Processes incoming JMS messages related to training resources and interoperability records.
+
+    The function extracts relevant information from the message and performs actions based on the message
+    content such as creating, updating, or deleting documents in the collection.
+
+    Args:
+        frame: The JMS message frame containing details like action type, data, and status.
+    """
     action = frame.headers["destination"].split(".")[-1]
     raw_collection = frame.headers["destination"].split("/")[-1].split(".")[0]
     frame_body = json.loads(frame.body)
@@ -22,6 +30,29 @@ def process_message(frame) -> None:
     suspended = frame_body["suspended"]
     status = frame_body["status"]
 
+    collection, data, data_id = extract_data_from_frame(raw_collection, frame_body)
+
+    logger.info(f"Started to process message, type: {raw_collection}, id: {data_id}")
+
+    if action == "create":
+        handle_create_action(active, suspended, status, collection, data, data_id)
+    elif action == "update":
+        handle_update_action(active, suspended, status, collection, data, data_id)
+    elif action == "delete":
+        handle_delete_action(collection, data_id, data)
+
+
+def extract_data_from_frame(raw_collection, frame_body):
+    """
+    Extracts data and collection information from the frame body.
+
+    Args:
+        raw_collection (str): The collection type from the JMS message.
+        frame_body (dict): The body of the JMS message containing the data.
+
+    Returns:
+        tuple: Returns a tuple containing collection, data, and data_id.
+    """
     if raw_collection == "training_resource":
         collection = settings.TRAINING
         data = frame_body["trainingResource"]
@@ -35,30 +66,64 @@ def process_message(frame) -> None:
         data = None
         data_id = None
 
-    logger.info(f"Received message, type: {raw_collection}, id: {data_id}")
+    return collection, data, data_id
 
-    if action == "create":
-        if (
-            active
-            and not suspended
-            and status in ["approved resource", "approved interoperability record"]
-        ):
-            logger.info(f"Create action - type: {raw_collection}, id: {data_id}")
-            transform_batch.delay(collection, data, full_update=False)
-    elif action == "update":
-        if (
-            active
-            and not suspended
-            and status in ["approved resource", "approved interoperability record"]
-        ):
-            logger.info(f"Update action - type: {raw_collection}, id: {data_id}")
-            transform_batch.delay(collection, data, full_update=False)
-        else:
-            if check_document_exists(collection, data_id):
-                logger.info(f"Delete action - type: {raw_collection}, id: {data_id}")
-                delete_data_by_id.delay(collection, data)
 
-    elif action == "delete":
+def handle_create_action(active, suspended, status, collection, data, data_id):
+    """
+    Handles the 'create' action for the JMS message.
+
+    Args:
+        active (bool): Flag indicating if the record is active.
+        suspended (bool): Flag indicating if the record is suspended.
+        status (str): The status of the record.
+        collection (str): The name of the collection.
+        data (dict): The data to be processed.
+        data_id (str): The ID of the data.
+    """
+    if (
+        active
+        and not suspended
+        and status in ["approved resource", "approved interoperability record"]
+    ):
+        logger.info(f"Creating action - {collection=}, ID: {data_id}")
+        transform_batch.delay(collection, data, full_update=False)
+
+
+def handle_update_action(active, suspended, status, collection, data, data_id):
+    """
+    Handles the 'update' action for the JMS message.
+
+    Args:
+        active (bool): Flag indicating if the record is active.
+        suspended (bool): Flag indicating if the record is suspended.
+        status (str): The status of the record.
+        collection (str): The name of the collection.
+        data (dict): The data to be processed.
+        data_id (str): The ID of the data.
+    """
+    if (
+        active
+        and not suspended
+        and status in ["approved resource", "approved interoperability record"]
+    ):
+        logger.info(f"Update action - {collection=}, ID: {data_id}")
+        transform_batch.delay(collection, data, full_update=False)
+    else:
         if check_document_exists(collection, data_id):
-            logger.info(f"Delete action - type: {raw_collection}, id: {data_id}")
+            logger.info(f"Delete action - {collection=}, ID: {data_id}")
             delete_data_by_id.delay(collection, data)
+
+
+def handle_delete_action(collection, data_id, data):
+    """
+    Handles the 'delete' action for the JMS message.
+
+    Args:
+        collection (str): The name of the collection.
+        data_id (str): The ID of the data to be deleted.
+        data (dict): The data to be deleted.
+    """
+    if check_document_exists(collection, data_id):
+        logger.info(f"Delete action - {collection=}, ID: {data_id}")
+        delete_data_by_id.delay(collection, data)
