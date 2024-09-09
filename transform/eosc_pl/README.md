3797507394d582160930c9359ce3dc996cfe5de2
mich.kolomanski <mich.kolomanski@gmail.com>
2024-09-09 13:03:01 +0200

    fix: solr multiple cols send

--- transform/eosc_pl/README.md
+++ transform/eosc_pl/README.md
@@ -3,5 +3,5 @@
 ### .env variables:
 - `DATASET_ADDRESS` - A dataset url address.
 - `SOLR_URL` - - Solr address. Default: `http://149.156.182.2:8983`.
-- `SOLR_EOSCPL_DATASET_COL_NAME` -  The name of the collection to which datasets will be sent.
+- `SOLR_EOSCPL_DATASET_COLS_NAME` -  The name of the collection to which datasets will be sent.
 <br></br>
--- transform/eosc_pl/transform/utils/send.py
+++ transform/eosc_pl/transform/utils/send.py
@@ -3,7 +3,7 @@
 from logging import getLogger
 import requests
 from requests.exceptions import ConnectionError as ReqConnectionError
-from eosc_pl.transform.utils.config import SOLR_URL, SOLR_EOSCPL_DATASET_COL_NAME
+from eosc_pl.transform.utils.config import SOLR_URL, SOLR_EOSCPL_DATASET_COLS_NAME
 
 logger = getLogger(__name__)
 
@@ -13,20 +13,21 @@ def send_json_string_to_solr(
     conf: dict,
 ) -> None:
     """Send data as a json string to solr"""
-    url = f"{conf[SOLR_URL]}/solr/{conf[SOLR_EOSCPL_DATASET_COL_NAME]}/update?commitWithin=100"
-    req_headers = {"Accept": "application/json", "Content-Type": "application/json"}
+    for col_name in conf[SOLR_EOSCPL_DATASET_COLS_NAME].split(" "):
+        url = f"{conf[SOLR_URL]}/solr/{col_name}/update?commitWithin=100"
+        req_headers = {"Accept": "application/json", "Content-Type": "application/json"}
 
-    try:
-        req = requests.post(url, data=data, headers=req_headers, timeout=180)
-        if req.status_code == 200:
-            logger.info(
-                f"Solr update was successful. Collection name={conf[SOLR_EOSCPL_DATASET_COL_NAME]}, status={req.status_code}"
-            )
-        else:
+        try:
+            req = requests.post(url, data=data, headers=req_headers, timeout=180)
+            if req.status_code == 200:
+                logger.info(
+                    f"Solr update was successful. Collection name={col_name}, status={req.status_code}"
+                )
+            else:
+                logger.error(
+                    f"Solr update has failed. Collection name={col_name}, status={req.status_code}"
+                )
+        except ReqConnectionError as e:
             logger.error(
-                f"Solr update has failed. Collection name={conf[SOLR_EOSCPL_DATASET_COL_NAME]}, status={req.status_code}"
+                f"Solr update has failed. Collection name={col_name}, error={e}"
             )
-    except ReqConnectionError as e:
-        logger.error(
-            f"Solr update has failed. Collection name={conf[SOLR_EOSCPL_DATASET_COL_NAME]}, error={e}"
-        )
