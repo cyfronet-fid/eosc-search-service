ddcb99cb32b28ad39ea8cd86a2aae22f14e9409d
mich.kolomanski <mich.kolomanski@gmail.com>
2024-09-17 14:34:18 +0200

    feat: landing page for the rds

--- ui/apps/ui/src/app/pages/landing-page/landing-page.component.ts
+++ ui/apps/ui/src/app/pages/landing-page/landing-page.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { DEFAULT_COLLECTION_ID } from '@collections/data';
-import { ConfigService } from '../../services/config.service';
-import { DEFAULT_SORT } from '@components/sort-by-functionality/sort-value.type';
 
 @Component({
   selector: 'ess-landing-page',
@@ -9,21 +7,5 @@ import { DEFAULT_SORT } from '@components/sort-by-functionality/sort-value.type'
   styleUrls: ['./landing-page.component.scss'],
 })
 export class LandingPageComponent {
-  marketplaceUrl = this._config.get().marketplace_url;
   allUrlPath = '/search/' + DEFAULT_COLLECTION_ID;
-  researchProductsPath = '/search/'; // TODO: set publications url
-  trainingsPath = '/search/'; // TODO: set trainings url
-
-  constructor(private _config: ConfigService) {}
-
-  fqBy(filterName: string, value: string) {
-    return {
-      q: '*',
-      sort_ui: DEFAULT_SORT,
-      fq: [`${filterName}:"${value}"`],
-    };
-  }
-  scroll(el: HTMLElement) {
-    el.scrollIntoView();
-  }
 }
